6e3d6cea04042a23a3b4a221d121a8ac0bf5a184
jl118 <jen.liebelt@gmail.com>
2022-02-15 14:50:35 -0500

    imported initial components and dependencies to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// imports
+import { Switch, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { lazy, Suspense } from "react";
+import GlobalStyle from "./globalStyles";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import Loading from "";
+
+// components
+const Main = lazy(() => import(""));
+const AboutPage = lazy(() => import(""));
+const ResumePage = lazy(() => import(""));
+const WorkPage = lazy(() => import(""));
+const ContactPage = lazy(() => import(""));
+const SoundBar = lazy(() => import(""));
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <Suspense fallback={<Loading />}>
+          <SoundBar />
+
+          <AnimatePresence exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route exact path="/" component={Main} />
+
+              <Route exact path="/about" component={AboutPage} />
+
+              <Route exact path="/resume" component={ResumePage} />
+
+              <Route exact path="/work" component={WorkPage} />
+
+              <Route exact path="/contact" component={ContactPage} />
+            </Switch>
+          </AnimatePresence>
+        </Suspense>
+      </ThemeProvider>
+    </>
   );
 }
 
